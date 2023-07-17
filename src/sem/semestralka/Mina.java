5f2b9338c78a0ab706b913728385d9808bea45e3
Lukas <lukas.kandera@gmail.com>
2023-07-17 15:59:13 +0200

    init mina

---
+++ src/sem/semestralka/Mina.java
@@ -0,0 +1,39 @@
+package sem.semestralka;
+
+public class Mina {
+    private int id;
+    private int pozX;
+    private int pozY;
+    private boolean odkryta;
+
+    public Mina(int id, int pozX, int pozY) {
+        this.id = id;
+        this.pozX = pozX;
+        this.pozY = pozY;
+        this.odkryta = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPozX() {
+        return pozX;
+    }
+
+    public void setPozX(int pozX) {
+        this.pozX = pozX;
+    }
+
+    public int getPozY() {
+        return pozY;
+    }
+
+    public void setPozY(int pozY) {
+        this.pozY = pozY;
+    }
+}
