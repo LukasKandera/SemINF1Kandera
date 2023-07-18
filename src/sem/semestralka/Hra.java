733e9e56debbf3c8c46f413ad8ca19c98607ca17
Lukas <lukas.kandera@gmail.com>
2023-07-18 14:25:46 +0200

    zaciatok generovania hracej plochy a min

---
+++ src/sem/semestralka/Hra.java
@@ -0,0 +1,17 @@
+package sem.semestralka;
+
+public class Hra {
+    private HraciaPlocha hraciaPlocha;
+
+    public Hra(int xMax, int yMax, int pocetMine) {
+        this.hraciaPlocha = new HraciaPlocha(xMax, yMax, pocetMine);
+    }
+
+    public void vlajka() {
+
+    }
+    public void odkryPole() {
+
+    }
+
+}
--- src/sem/semestralka/HraciaPlocha.java
+++ src/sem/semestralka/HraciaPlocha.java
@@ -1,14 +1,40 @@
 package sem.semestralka;
 
+import java.util.Random;
+
 public class HraciaPlocha {
     private int xMax;
     private int yMax;
     private int pocetMine;
+    private Mina[] miny;
+    private int[][] plocha;
 
-    public HraciaPlocha(int xMax, int yMax, int pocetMine) {
-        this.xMax = xMax;
-        this.yMax = yMax;
-        this.pocetMine = pocetMine;
+    public HraciaPlocha(int pxMax, int pyMax, int pPocetMine) {
+        this.xMax = pxMax;
+        this.yMax = pyMax;
+        this.pocetMine = pPocetMine;
+        this.miny = new Mina[this.pocetMine];
+        this.plocha = new int[this.xMax][this.yMax];
+        this.vygenerujPlochu();
+    }
+    public void vygenerujPlochu() {
+        for (int i = 0; i < this.xMax; i++) {
+            for (int j = 0; j < this.yMax; j++) {
+                this.plocha[i][j] = 0;
+            }
+        }
+        for (int i = 0; i < this.pocetMine; i++) {
+            Random rand = new Random();
+
+            int x = rand.nextInt(this.xMax);
+            int y = rand.nextInt(this.yMax);
+            this.miny[i] = new Mina(i, x, y);
+            //kontrola
+
+
+            this.plocha[x][y] = 9;
+        }
+
     }
 
     public int getxMax() {
